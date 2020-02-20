23f06bfa5cde3b1fa7fe062df627539151ae792c
Yutong Li <rainbowzom@gmail.com>
2020-02-20 00:45:15 -0800

    added some unicode

--- Quizzes.cpp
+++ Quizzes.cpp
@@ -5,8 +5,8 @@
 
 std::unordered_map<std::string, std::string> quizList[10];
 std::unordered_map<std::string, std::string> Lesson3({
-                                                             {"One", "一"},
-                                                             {"Two", "二"},
+                                                             {"One", "\u4e00"},
+                                                             {"Two", "\u2f06"},
                                                              {"Three", "三"},
                                                              {"Four","四"},
                                                              {"Five", "五"},
@@ -21,18 +21,51 @@ std::unordered_map<std::string, std::string> Lesson3({
                                                              {"Yen (Japanese money)", "円"},
                                                              {"Time", "時"},
                                                      });
+//std::unordered_map<std::string, std::string> Lesson4({
+//                                                             {"Day/Sun", "日"},
+//                                                             {"Book/Basis", "本"},
+//                                                             {"Person", "人"},
+//                                                             {"Month/Moon","月"},
+//                                                             {"Fire", "火"},
+//                                                             {"Water", "水"},
+//                                                             {"Tree", "木"},
+//                                                             {"Gold", "金"},
+//                                                             {"Soil", "土"},
+//                                                             {"Weekday", "曜"},
+//                                                             {"Up", "上"},
+//                                                             {"Down", "下"},
+//                                                             {"Middle", "中"},
+//                                                             {"Half", "半"}
+//                                                     });
+//std::unordered_map<std::string, std::string> Lesson5({
+//                                                             {"One", "一"},
+//                                                             {"Two", "二"},
+//                                                             {"Three", "三"},
+//                                                             {"Four","四"},
+//                                                             {"Five", "五"},
+//                                                             {"Six", "六"},
+//                                                             {"Seven", "七"},
+//                                                             {"Eight", "八"},
+//                                                             {"Nine", "九"},
+//                                                             {"Ten", "十"},
+//                                                             {"Hundred", "百"},
+//                                                             {"Thousand", "千"},
+//                                                             {"Ten Thousand", "万"},
+//                                                             {"Yen (Japanese money)", "円"},
+//                                                             {"Time", "時"}
+//                                                     });
 
 void Quizzes::startUp() {
     quizList[0] = Lesson3;
-    quizList[1] = Lesson4;
-    quizList[2] = Lesson5;
-    quizList[3] = Lesson6;
-    quizList[4] = Lesson7;
-    quizList[5] = Lesson8;
-    quizList[6] = Lesson9;
-    quizList[7] = Lesson10;
-    quizList[8] = Lesson11;
-    quizList[9] = Lesson12;
+//    quizList[1] = Lesson4;
+//    quizList[2] = Lesson5;
+//    quizList[3] = Lesson6;
+//    quizList[4] = Lesson7;
+//    quizList[5] = Lesson8;
+//    quizList[6] = Lesson9;
+//    quizList[7] = Lesson10;
+//    quizList[8] = Lesson11;
+//    quizList[9] = Lesson12;
 }
 
 std::unordered_map<std::string, std::string> Quizzes::getQuiz(int input) {
--- main.cpp
+++ main.cpp
@@ -4,27 +4,51 @@
 #include <cassert>
 #include "Quizzes.cpp"
 #include "Quizzes.h"
+#include <cstdlib>
+#include <locale.h>
 
 std::string userInput;
 int userScore = 0;
+int totalScore;
 
 
 std::unordered_map<std::string, std::string> selectedQuiz;
 
+void mainQuiz();
+
 int main() {
+    mainQuiz();
+}
+
+void mainQuiz() {
     Quizzes* quizzes = new Quizzes();
     quizzes->startUp();
 
     std::cout << "Welcome To Kanji Quiz for Genki I! \n" << "Please enter the lesson number (3-12) you wish to review. \n";
     std::cin >> userInput;
     selectedQuiz = quizzes->getQuiz(std::stoi(userInput));
-    
-
-
-
-
-
-
-
-
+    totalScore = selectedQuiz.size();
+    for (std::unordered_map<std::string, std::string>::iterator i = selectedQuiz.begin();
+    i !=selectedQuiz.end(); i++){
+        std::cout << "Please type the Kanji for " << i->first <<std::endl;
+        std::cin >> userInput;
+        if (userInput == i->second){
+            std::cout << "Right answer!" << std::endl;
+            userScore++;
+        }
+        else{
+            std::cout << "Wrong answer!" <<std::endl;
+            std::cout << "The correct answer is " << i->second << std::endl;
+        }
+    }
+    std::cout << "Your final score is: " << userScore << " out of " << totalScore << std::endl;
+    std::cout << "Press enter to exit, or enter \"r\" to do another quiz";
+    std::cin >> userInput;
+    if (userInput == "r"){
+        mainQuiz();
+        userScore = 0;
+    }
+    else{
+        exit(0);
+    }
 }
