a896556e3b7b462283bd26d59f373fff5741cf82
Yutong Li <rainbowzom@gmail.com>
2020-02-19 17:02:08 -0800

    first

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
-
+#include <unordered_map>
 #include <string>
 #include <cassert>
-std::string userAnswer;
+std::string userInput;
 int userScore = 0;
 
+std::list<std::unordered_map<std::string, std::string>> quizList;
 
 int main() {
-    std::cout << "Welcome To Kanji Quiz for Genki I! Please enter the lesson number you wish to review.";
+    std::cout << "Welcome To Kanji Quiz for Genki I! \n" << "Please enter the lesson number you wish to review. \n";
+    std::cin >> userInput;
+
+    
 
 
 }
