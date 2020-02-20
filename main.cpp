a9099f2274012c0e60830ac929af46d4be015bf6
Yutong Li <rainbowzom@gmail.com>
2020-02-19 16:26:56 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <string>
+#include <cassert>
+std::string userAnswer;
+int userScore = 0;
+
+
+int main() {
+    std::cout << "Welcome To Kanji Quiz for Genki I! Please enter the lesson number you wish to review.";
+
+
+}
